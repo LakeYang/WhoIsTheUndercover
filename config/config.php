b56df1fe021c62a7d3b422dea8bfbc0f363c4a53
LakeYang <hmnotehuyang@gmail.com>
2016-06-11 21:37:56 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php 
+define("APPID","WeChatAppid");
+define("TOKEN","WeChatToken");
+define("SECRET","WeChatSecret");
+mysql_connect("localhost","DB_USERNAME","DB_PASSWORD");
+mysql_select_db('DB_NAME');
+?>
