756fe42bf04391afbdd32d1a9be8e4155167bd12
LakeYang <hmnotehuyang@gmail.com>
2016-06-23 20:54:48 +0800

    coding...

--- config/config.php
+++ config/config.php
@@ -7,4 +7,5 @@ define("DB_USERNAME","game_user");
 define("DB_PASSWORD","123456");
 define("DB_NAME","game");
 define("LANGUAGE","zh_CN");
+define("WechatEnabled",true);
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,25 @@ include_once dirname(__FILE__).'/config/loader.php';
 <html>
 <head>
 <meta charset="utf-8">
+<!--
+This is an open-source web game by LakeYang
+GitHub Page: githun.com/LakeYang/WhoIsTheUndercover.git
+Copyright LakeYang(hihuyang.com) 2016
+Licensed under the Apache License, Version 2.0
+-->
 <title><?php echo trans('Who is the undercover'); ?></title>
+<script src="js/libs/jquery-3.0.0.min.js"></script>
+<script src="js/main.js"></script>
+<?php 
+if(WechatEnabled){
+?>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<?php 
+}
+?>
+<script>
+var WechatEnabled=<?php if(WechatEnabled)echo 1;else echo 0; ?>;
+</script>
 </head>
 
 <body>
