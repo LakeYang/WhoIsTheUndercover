cffe5d2a5c2853cd45b23013782b9dbeea3011b8
zingpie <orangelhao@gmail.com>
2016-07-03 19:49:44 +0800

    Revert "Coding..."
    This reverts commit 0062fd64b932714b16f758d49abe01c8105d136b.

--- js/main.js.php
+++ js/main.js.php
@@ -13,16 +13,5 @@ The index.php just show a loading progress bar, then all the thing is send to th
 Note because this application is multi-language, so language is injected to this via php.
 */
 $(document).ready(function(){
-	var queue = new createjs.LoadQueue();
-	queue.installPlugin(createjs.Sound);
-	queue.on("complete",function(){
-		$(".enter-btn").css("background","#3498db");
-		$(".enter-btn").val("<?php echo trans('Enter'); ?>");
-	}, this);
-	queue.on("progress", function(){
-		$(".loading_progress").css("width",queue.progress*100+"%");
-	});
-	queue.loadManifest([
-		{id:"jweixin", src:"js/libs/jweixin-1.0.0.js"}
-	]);
+	$("#top_ui").hide();
 });
