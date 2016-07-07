f1f11457ee7e4e2ec12987d697992a5e78b6ca9b
LakeYang <hmnotehuyang@gmail.com>
2016-07-07 19:51:52 +0800

    config update

--- config/config.php
+++ config/config.php
@@ -6,6 +6,7 @@ define("HOSTNAME","localhost");
 define("DB_USERNAME","game_user");
 define("DB_PASSWORD","123456");
 define("DB_NAME","game");
+define("TABLE_PREFIX","wx");
 define("LANGUAGE","zh_CN");
 define("WechatEnabled",true);
 ?>
